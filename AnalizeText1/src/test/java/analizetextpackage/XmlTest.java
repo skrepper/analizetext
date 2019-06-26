92de2f46cf1665ea3d119e9fb699600cd4b7db39
Цыден <skrepper@yandex.ru>
2019-06-26 22:18:09 +0700

    Проверка xml файла при помощи xsd

--- AnalizeText1/src/test/java/analizetextpackage/XmlTest.java
+++ AnalizeText1/src/test/java/analizetextpackage/XmlTest.java
@@ -2,72 +2,82 @@ package analizetextpackage;
 
 import org.junit.Assert;
 import org.junit.Test;
- 
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.*;
+import javax.xml.bind.util.JAXBSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.StringReader;
 
 public class XmlTest {
-    private static String TEST_XML="<model>" + 
-    		"  <rules>" + 
-    		"    <rule>" + 
-    		"      <factExpression>spring</factExpression>" + 
-    		"      <resultingFact>" + 
-    		"        rain" + 
-    		"      </resultingFact>" + 
-    		"    </rule>" + 
-    		"    <rule>" + 
-    		"      <andExpression>" + 
-    		"        <factExpression>spring</factExpression>" + 
-    		"        <factExpression>winter</factExpression>" + 
-    		"      </andExpression>" + 
-    		"      <resultingFact>" + 
-    		"        autumn" + 
-    		"      </resultingFact>" + 
-    		"    </rule>" + 
-    		"    <rule>" + 
-    		"      <orExpression>" + 
-    		"        <factExpression>spring</factExpression>" + 
-    		"        <factExpression>winter</factExpression>" + 
-    		"        <andExpression>" + 
-    		"          <factExpression>summer</factExpression>" + 
-    		"          <factExpression>rain</factExpression>" + 
-    		"        </andExpression>" + 
-    		"      </orExpression>" + 
-    		"      <resultingFact>" + 
-    		"        autumn" + 
-    		"      </resultingFact>" + 
-    		"    </rule>" + 
-    		"  </rules>" + 
-    		"  <approvedFacts>" + 
-    		"    <approvedFact fact=\"spring\"/>" + 
-    		"    <approvedFact fact=\"summer\"/>" + 
-    		"  </approvedFacts>" + 
-    		"</model>" + 
-    		"";
      
     @Test
-    public void test() throws JAXBException {
+    public void test() throws JAXBException, SAXException, IOException {
         JAXBContext jc = JAXBContext.newInstance(
-        		Model.class, 
+        		Model.class/*, 
         		Rule.class, 
         		FactExpression.class,
-        		AndExpression.class,
-        		OrExpression.class
+        		AndExpression.class ,
+        		OrExpression.class*/
         		);
         Unmarshaller unmarshaller = jc.createUnmarshaller();
  
         Model result = (Model)
-                unmarshaller.unmarshal(new StringReader(TEST_XML));
+                unmarshaller.unmarshal(new FileReader(new File("src/test/resources/func_xml_1.xml") ));
+
+        JAXBSource source = new JAXBSource(jc, result);
+        
+        SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI); 
+        Schema schema = sf.newSchema(new File("src/main/resources/func_xml.xsd")); 
+
+        Validator validator = schema.newValidator();
+        validator.setErrorHandler(new MyErrorHandler());
+        validator.validate(source);
         
         Marshaller marshaller = jc.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.marshal(result, System.out);
         Assert.assertEquals(1, 1);
     }
+    
+    
+    
+    public class MyErrorHandler implements ErrorHandler {
+    	 
+		@Override
+        public void warning(SAXParseException exception) throws SAXException {
+            System.out.println("\nWARNING");
+            exception.printStackTrace();
+        }
+     
+		@Override
+        public void error(SAXParseException exception) throws SAXException {
+            System.out.println("\nERROR");
+            exception.printStackTrace();
+        }
+     
+		@Override
+        public void fatalError(SAXParseException exception) throws SAXException {
+            System.out.println("\nFATAL ERROR");
+            exception.printStackTrace();
+        }
+
+     
+    }
+
 
 }
